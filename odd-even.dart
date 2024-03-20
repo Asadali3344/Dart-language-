d3d29d48d75d166169d04c1fa1e1d04fb28e8055
Asadali3344 <161151573+Asadali3344@users.noreply.github.com>
2024-03-21 01:45:49 +0500

    even-odd.dart
    This program checks whether a given number is even or odd.

---
+++ odd-even.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  int num;
+  stdout.write('Enter the numeber:');
+  num = int.parse(stdin.readLineSync()!);
+
+  if (num % 2 == 0) {
+    print('\n The number is even:$num');
+  } else {
+    print('\n The number is odd $num');
+  }
+}
