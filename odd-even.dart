52e206a9b9d850039494e8c98eec5b596e4b4026
Asadali3344 <161151573+Asadali3344@users.noreply.github.com>
2024-03-21 01:47:22 +0500

    Update odd-even.dart

--- odd-even.dart
+++ odd-even.dart
@@ -7,8 +7,8 @@ void main() {
   num = int.parse(stdin.readLineSync()!);
 
   if (num % 2 == 0) {
-    print('\n The number is even:$num');
+    print('\n The number is even: $num');
   } else {
-    print('\n The number is odd $num');
+    print('\n The number is odd: $num');
   }
 }
