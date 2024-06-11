b347551ab7f8e299a74e532da9d42c73a8ccc335
Asadali3344 <161151573+Asadali3344@users.noreply.github.com>
2024-06-12 03:55:17 +0500

    swapping
    swapping the values.

---
+++ swapping.dart
@@ -0,0 +1,13 @@
+void main() {
+  int a = 5;
+  int b = 10;
+  print('the value is not swiped:');
+  int c;
+  c = a;
+  a = b;
+  b = c;
+  print('swiped the numebr:');
+
+  print("a: $a");
+  print("b: $b");
+}
